dae061078e3c96572d3f020b03c25a30aca1f8f7
anyongjin <anyongjin163@163.com>
2023-07-09 15:42:29 +0800

    默认使用浏览器端语言

--- composables/i18n.ts
+++ composables/i18n.ts
@@ -1,11 +1,29 @@
 import {createI18n} from "vue-i18n";
 import cn from "~/locales/cn.json";
 import en from "~/locales/en.json";
+import {useStorage} from "@vueuse/core";
 
-const i18n = createI18n({
+
+export const useUserLang = (): string => {
+  const lang = useStorage<string | undefined>('lang', undefined)
+  if(!lang.value && process.client){
+    const nav = navigator as any;
+    const userLang: string = nav.language || nav.userLanguage || 'en-US';
+    if(userLang.startsWith('zh-')){
+      lang.value = 'cn'
+    }
+    else{
+      lang.value = 'en'
+    }
+  }
+  return lang.value ?? 'en'
+}
+
+
+export const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: 'cn',
+  locale: useUserLang(),
   messages: {
     cn, en
   }
